42876224b60e2d149dc2ecf18c5343373711bc01
jsonrn36 <jason.rn36@gmail.com>
2026-02-22 13:21:39 -0400

    feito o salvamento da branch

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
 document.getElementById("texto").innerHTML="Meu Primeiro Texto em <b>Javascript</b>! </br> Com document.ElementByID.";
-=======
-document.getElementById("texto").innerHTML="Aula 02 Meu console.log em <b>Javascript</b>! </br> Para Visualisar o console log tecle F12, ou </br> Clique com o botão direito do mouse,</br> depois em inspecionar elemento";
-console.log("Olá isso é um console Log");
->>>>>>> d5ff6e3a4d507bfbd430621c093d3c754400822d
+
+
